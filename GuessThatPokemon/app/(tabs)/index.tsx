41a13f6950bbe1906d6c02860d3a3eaa9fa9a175
Sebastian Echeverria <secheverriamartinez@csumb.edu>
2025-02-01 02:26:12 -0800

    Added basic game functionality
    I added a streak counter that adds 1 for each correct answer
    
    Buttons are clickable with pokemons name
    
    Randomly select between Pokemon's image, cry, or pokedex entry (to make it more difficult for the player to guess as they progress)
    
    After a streak of 5 images or cries begin to appear
    
    After a streak of 10 images, cries, or pokedex numbers begin to appear

--- GuessThatPokemon/app/(tabs)/index.tsx
+++ GuessThatPokemon/app/(tabs)/index.tsx
@@ -3,11 +3,15 @@ import {useState, useEffect} from "react"
 
 import { Audio } from 'expo-av'
 
-var pokemonArray = [];
 
 export default function HomeScreen() {
+  var pokemonArray = [];
   const [pokemons, setPokemonArray] = useState([]);
+
+
+  
   const dataFetch = async () => {
+    setPokemonArray([]);
     for(var i = 0; i < 4; i++){
       var randomPokedexNum = Math.floor(Math.random() * 1025) + 1;
       console.log(randomPokedexNum)
@@ -15,39 +19,109 @@ export default function HomeScreen() {
       console.log(url);
       const response = await fetch(url);
       const data = await response.json();
-    // setData(data);
+      // setData(data);
       pokemonArray.push(data)
     }
     // const response = await fetch(url, options);
-  setPokemonArray(pokemonArray)
+    setPokemonArray(pokemonArray)
   }
+
   useEffect(() => {
     dataFetch()
   }, []);
+  
+  const [pokemonToGuess, setPokemonToGuess] = useState();
 
 
 
+  const getPokemonToGuess = () => {
+    var pickRandomPokemon = Math.floor(Math.random() * pokemons.length)
+    var pokemonToGuess = pokemons[pickRandomPokemon];
+
+    setPokemonToGuess(pokemonToGuess)
+
+  }
+
+  useEffect(() => {
+    if(pokemons.length >= 4) {
+      getPokemonToGuess()
+    }
+  }, [pokemons])
 
   
 
+  const placePokemonToGuessImage = (isCryActive) => {
+    if(!isCryActive){
+      if(pokemonToGuess != undefined){
+        return <Image style={{height: 100, width: 100}} source = {{ uri: pokemonToGuess.sprites.front_default }}/>
+      }
+    }
+  }
+
+  const placePokemonPokedexEntry = () => {
+    if(pokemonToGuess != undefined){
+      return <Text style={{ fontSize: 25, color: "#ffffff"}}> Pokedex Number: {pokemonToGuess.id} </Text>
+    }
+  }
+
+  console.log("PokemonToGuess: ", pokemonToGuess)
+
+  const clearArray = () => {
+    console.log("POKEMONS LENGTH: ", pokemons.length)
+    for(var i = 0; i < pokemons.length; i++){
+      pokemons.pop()
+    }
+  }
+
+  const [streak, setStreak] = useState(0);
+
+  const correctPokemon = (pokemonGuessed) => {
+    if(pokemonToGuess.name == pokemonGuessed.name){
+      setStreak(streak + 1);
+    }else{
+      setStreak(0);
+    }
+
+    dataFetch()
+
+  }
+
+  const placeButtons = () => {
+    var randomNum = Math.floor(Math.random() * 10);
+    console.log(randomNum)
+    if(pokemons.length >= 4){
+      if(streak >= 5 && randomNum >= 4 && randomNum <= 6){
+          return <View> <br></br> {placePokemonToGuessImage(true)} <Button title='PLAY POKEMON CRY' onPress={() => playSound(pokemonToGuess.cries.latest)}/> <br></br> <Button title={pokemons[0].name} onPress={() => correctPokemon(pokemons[0])}/>
+          <Button title={pokemons[1].name} onPress={() => correctPokemon(pokemons[1])}/>
+          <Button title={pokemons[2].name} onPress={() => correctPokemon(pokemons[2])}/>
+          <Button title={pokemons[3].name} onPress={() => correctPokemon(pokemons[3])}/></View>
+      }else if(streak >= 10 && randomNum > 7){
+        return <View> <br></br> {placePokemonPokedexEntry()} <br></br> <Button title={pokemons[0].name} onPress={() => correctPokemon(pokemons[0])}/>
+        <Button title={pokemons[1].name} onPress={() => correctPokemon(pokemons[1])}/>
+        <Button title={pokemons[2].name} onPress={() => correctPokemon(pokemons[2])}/>
+        <Button title={pokemons[3].name} onPress={() => correctPokemon(pokemons[3])}/></View>
+
+      }else{
+        return <View> <br></br> {placePokemonToGuessImage(false)} <br></br> <Button title={pokemons[0].name} onPress={() => correctPokemon(pokemons[0])}/>
+          <Button title={pokemons[1].name} onPress={() => correctPokemon(pokemons[1])}/>
+          <Button title={pokemons[2].name} onPress={() => correctPokemon(pokemons[2])}/>
+          <Button title={pokemons[3].name} onPress={() => correctPokemon(pokemons[3])}/></View>
+      }
+    }
+  }
+
+  useEffect(() => {
+    placeButtons()
+  });
+
+  
   // useEffect(() => {
-  //   dataFetch()
-  // }, []);
+  //   placePokemonToGuessImage(false)
+  // });
 
-  // console.log()
-  console.log(pokemons[0].cries.latest);
-  // console.log(pokemons[0].name)
-  //
-
-  // SoundPlayer.playUrl("")
-
-  // var audio = `${pokemons[0].cries.latest}`;
-
-  // SoundPlayer.loadUrl(audio);
   
 
-  // console.log(pokemonArray[0].cries.latest)
-  //SoundPlayer.playUrl(`${pokemon.cries.latest}`)
+
   const playSound = async (pokemonURL) => {
     const { sound } = await Audio.Sound.createAsync(
       { uri: pokemonURL },
@@ -55,68 +129,17 @@ export default function HomeScreen() {
     )
   }
 
+  console.log(pokemons)
 
+  // if(pokemonToGuess != undefined)
 
   return (
     <View>
-      <View>
-      </View>
-      {pokemons.map((pokemon, index) => (
-        <View> 
-          <Text style={{ fontSize: 25, color: "#ffffff"}}>
-            {pokemon.name}
-          </Text>
-          <Button title='PLAY POKEMON CRY' onPress={() => playSound(pokemon.cries.latest)}/> 
-          <Image style={{height: 100, width: 100}} source = {{ uri: pokemon.sprites.front_default }}/>
-        </View>
-      ))}
+      <Text style={{ fontSize: 25, color: "#ffffff"}}>
+        Streak: {streak}
+      </Text>
+          {placeButtons()}
     </View>
-
-    
-    // <ParallaxScrollView
-    //   headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-    //   headerImage={
-    //     <Image
-    //       source={require('@/assets/images/partial-react-logo.png')}
-    //       style={styles.reactLogo}
-    //     />
-    //   }>
-    //   <ThemedView style={styles.titleContainer}>
-    //     <ThemedText type="title">Welcome!</ThemedText>
-    //     <HelloWave />
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-    //     <ThemedText>
-    //       Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-    //       Press{' '}
-    //       <ThemedText type="defaultSemiBold">
-    //         {Platform.select({
-    //           ios: 'cmd + d',
-    //           android: 'cmd + m',
-    //           web: 'F12'
-    //         })}
-    //       </ThemedText>{' '}
-    //       to open developer tools.
-    //     </ThemedText>
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-    //     <ThemedText>
-    //       Tap the Explore tab to learn more about what's included in this starter app.
-    //     </ThemedText>
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-    //     <ThemedText>
-    //       When you're ready, run{' '}
-    //       <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-    //       <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-    //       <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-    //       <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-    //     </ThemedText>
-    //   </ThemedView>
-    // </ParallaxScrollView>
   );
 }
 
